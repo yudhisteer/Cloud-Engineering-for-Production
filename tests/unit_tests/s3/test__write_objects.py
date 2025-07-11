61ce87ef7af9df87c8cd8e7df3f5204c8aa46f9c
yudhisteer <yudhisteer.chin@gmail.com>
2025-07-11 15:58:44 -0700

    feat: changed test for write objects with mocked_aws fixture

--- tests/unit_tests/s3/test__write_objects.py
+++ tests/unit_tests/s3/test__write_objects.py
@@ -1,4 +1,5 @@
 import boto3
+from moto import mock_aws
 
 try:
     from mypy_boto3_s3.type_defs import (
@@ -12,10 +13,8 @@ from files_api.s3.write_objects import upload_s3_object
 from tests.consts import TEST_BUCKET_NAME
 
 
-def test__upload_s3_object() -> None:
-    # create a bucket
-    s3_client = boto3.client("s3")
-    s3_client.create_bucket(Bucket=TEST_BUCKET_NAME)
+@mock_aws
+def test__upload_s3_object(mocked_aws: None) -> None:
 
     # upload a file to the bucket, with a particular content type
     object_key = "test.txt"
@@ -29,14 +28,8 @@ def test__upload_s3_object() -> None:
     )
 
     # check that the file was uploaded with the correct content type
-    s3_client = boto3.client("s3")
+    s3_client = boto3.client("s3", region_name="us-east-1")
     response: GetObjectOutputTypeDef = s3_client.get_object(Bucket=TEST_BUCKET_NAME, Key=object_key)
     assert response["Body"].read() == file_content
     assert response["ContentType"] == content_type
     
-    # delete  all objects in the bucket and the bucket itself
-    list_response: ListObjectsV2OutputTypeDef = s3_client.list_objects_v2(Bucket=TEST_BUCKET_NAME)
-    for obj in list_response.get("Contents", []):
-        if "Key" in obj:
-            s3_client.delete_object(Bucket=TEST_BUCKET_NAME, Key=obj["Key"])
-    s3_client.delete_bucket(Bucket=TEST_BUCKET_NAME)
