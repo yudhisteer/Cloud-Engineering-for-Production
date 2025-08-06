f9866f11b624848466c71db1394d7a45cd290f20
yudhisteer <cyudhist@amazon.com>
2025-08-06 12:46:49 -0700

    feat: added all pydantic models for endpoints

---
+++ src/files_api/schemas.py
@@ -0,0 +1,38 @@
+# src/files_api/schemas.py
+from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel
+
+####################################
+# --- Request/response schemas --- #
+####################################
+
+
+# read (cRud)
+class FileMetadata(BaseModel):
+    file_path: str
+    last_modified: datetime
+    size_bytes: int
+
+# read (cRud)
+class GetFilesResponse(BaseModel):
+    files: List[FileMetadata]
+    next_page_token: Optional[str]
+
+
+# read (cRud)
+class GetFilesQueryParams(BaseModel):
+    page_size: int = 10
+    directory: Optional[str] = ""
+    page_token: Optional[str] = None
+
+
+# delete (cruD)
+class DeleteFileResponse(BaseModel):
+    message: str
+
+
+# create/update (CrUd)
+class PutFileResponse(BaseModel):
+    file_path: str
+    message: str
