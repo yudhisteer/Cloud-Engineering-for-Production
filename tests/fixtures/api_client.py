f13bac1f4d5c88b7c0ba9a2f9f47d6e9310cfcd9
yudhisteer <yudhisteer.chin@gmail.com>
2025-08-06 23:08:35 -0700

    refactor: move out fixture to separate file

---
+++ tests/fixtures/api_client.py
@@ -0,0 +1,16 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from files_api.settings import Settings
+from files_api.main import create_app
+from tests.consts import TEST_BUCKET_NAME
+
+
+
+# Fixture for FastAPI test client
+@pytest.fixture
+def client(mocked_aws) -> TestClient:  # pylint: disable=unused-argument
+    settings: Settings = Settings(s3_bucket_name=TEST_BUCKET_NAME)
+    app = create_app(settings=settings)
+    with TestClient(app) as client:
+        yield client
