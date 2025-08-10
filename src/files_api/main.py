7cf5a56bafc52e36ba964d8cb4d6d2b3ac52b1b6
yudhisteer <yudhisteer.chin@gmail.com>
2025-08-09 17:22:07 -0700

    feat: removed unused code and add middleware and exception handler

--- src/files_api/main.py
+++ src/files_api/main.py
@@ -1,42 +1,31 @@
 import os
-from datetime import datetime
-from typing import (
-    List,
-    Optional,
-)
 
-from fastapi import (
-    Depends,
-    FastAPI,
-    APIRouter,
-    HTTPException,
-    Response,
-    UploadFile,
-    status,
-    Request,
-)
-from fastapi.responses import StreamingResponse
-from botocore.exceptions import ClientError
+import pydantic
+from fastapi import FastAPI
+
 
 from files_api.settings import Settings
-from files_api.s3.delete_objects import delete_s3_object
-from files_api.s3.read_objects import (
-    fetch_s3_object,
-    fetch_s3_objects_metadata,
-    fetch_s3_objects_using_page_token,
-    object_exists_in_s3,
-)
-from files_api.s3.write_objects import upload_s3_object
 from files_api.routes import ROUTER
-
+from src.errors import handle_broad_exception, handle_pydantic_validation_errors
 
 
 def create_app(settings: Settings | None = None) -> FastAPI:
     """Create and return the FastAPI application instance."""
-    settings = settings or Settings()
+    # Use the provided settings or create a new Settings instance if none are given
+    settings = settings or Settings(s3_bucket_name=os.environ["S3_BUCKET_NAME"])
     app = FastAPI()
+    # Store the settings in the app's state for access throughout the app
     app.state.settings = settings
+    # Register the API router with the FastAPI app
     app.include_router(ROUTER)
+    # Add a custom exception handler for Pydantic validation errors
+    app.add_exception_handler(
+        exc_class_or_status_code=pydantic.ValidationError,
+        handler=handle_pydantic_validation_errors,
+    )
+    # Add a middleware to handle broad exceptions and return appropriate responses
+    app.middleware("http")(handle_broad_exception)
+    # Return the configured FastAPI application instance
     return app
 
 
